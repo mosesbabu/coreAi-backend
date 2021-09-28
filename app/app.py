fd5b94eaec1856200753041648b3e92c81c6c0b5
moselamar <44118179+moselamar@users.noreply.github.com>
2021-09-29 00:32:41 +0300

    added endpoint for data creation

--- app/app.py
+++ app/app.py
@@ -1,28 +1,46 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify,abort, make_response
 from flask_mysqldb import MySQL
  
  
 
 app = Flask(__name__)
+
+
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = ''
+app.config['MYSQL_DB'] = 'coreai'
+
 mysql = MySQL(app)
     
-@app.route('/api', methods = ['POST', 'GET'])
+@app.route('/api', methods = ['GET'])
 def index ():
-    if request.method == 'GET':
-        return "Login via the login Form"
+    
+    cursor = mysql.connection.cursor()
+    data=cursor.execute(" SELECT * FROM dataset") or []
+    if data:
+        return jsonify({
+                "amount": data.amount,
+                "date": data.date,
+            })
+    else:    
+        return {},404
+  
+    
      
 @app.route('/api/add', methods = ['POST'])
 def create():
     if request.method == 'POST':
-        amount = request.form['amount']
-        date = request.form['date']
+        amount = request.json['amount']
+        date = request.json['date']
         cursor = mysql.connection.cursor()
         cursor.execute(''' INSERT INTO dataset VALUES(%s,%s)''',(amount,date))
         mysql.connection.commit()
         cursor.close()
         return f"Done!!"
- 
-app.run(host='localhost', port=5000)   
+
+if __name__ == '__main__':
+   app.run(host='localhost', port=5000)   
  
 
 
