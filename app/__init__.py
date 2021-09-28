eaa70b550d1b96387a8c388cf4efb3c8ac662987
moselamar <44118179+moselamar@users.noreply.github.com>
2021-09-28 22:00:49 +0300

    fix config options error

--- app/__init__.py
+++ app/__init__.py
@@ -9,8 +9,7 @@ def create_app(config_name):
 
     app = Flask(__name__)
 
-    # Creating the app configurations
-    app.config.from_object(config_options[config_name])
+    
     
     #Initializing Flask Extensions
     db.init_app(app)
