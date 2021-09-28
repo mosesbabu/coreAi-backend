997ea8c21067385be43ad0a423cf8474137b9883
moselamar <44118179+moselamar@users.noreply.github.com>
2021-09-28 23:30:54 +0300

    added api routes

--- app/app.py
+++ app/app.py
@@ -1,8 +1,33 @@
-from flask import Flask
-def create_app():
-    app = Flask(__name__)
-    @app.route('/')
-    def hello_world():
-        return 'Hello, World!'
-    return app
-app = create_app()
+from flask import Flask, request, jsonify
+from flask_mysqldb import MySQL
+ 
+ 
+
+app = Flask(__name__)
+mysql = MySQL(app)
+    
+@app.route('/api', methods = ['POST', 'GET'])
+def index ():
+    if request.method == 'GET':
+        return "Login via the login Form"
+     
+@app.route('/api/add', methods = ['POST'])
+def create():
+    if request.method == 'POST':
+        amount = request.form['amount']
+        date = request.form['date']
+        cursor = mysql.connection.cursor()
+        cursor.execute(''' INSERT INTO dataset VALUES(%s,%s)''',(amount,date))
+        mysql.connection.commit()
+        cursor.close()
+        return f"Done!!"
+ 
+app.run(host='localhost', port=5000)   
+ 
+
+
+
+
+
+
+
