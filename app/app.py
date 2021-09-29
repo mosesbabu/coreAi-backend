0bb0ded21370a3fe72d79a7931547113999a079b
moselamar <44118179+moselamar@users.noreply.github.com>
2021-09-29 22:18:26 +0300

    fix add endpoint and db save

--- app/app.py
+++ app/app.py
@@ -4,6 +4,7 @@ import json
 import mysql.connector
 from mysql.connector import Error
 
+
 app = Flask(__name__)
 
 
@@ -45,25 +46,25 @@ try:
     @app.route('/api/retrieve', methods=['GET'])
     def get_data():
         return jsonify({'dataset': jsonData_list})
-    
     @app.route('/api/add', methods = ['POST'])
     def create():
-   
+        data_id=request.json['id']
         amount = request.json['amount']
         amount_one = request.json['amount_one']
         amount_two = request.json['amount_two']
         amount_three = request.json['amount_three']
         amount_four = request.json['amount_four']
         date = request.json['date']
-        cursor = mysql.connection.cursor()
-        cursor.execute(''' INSERT INTO dataset VALUES(%s,%s,%s,%s,%s,%s)''',(amount,amount_one,amount_two,amount_three,amount_four,date))
-        mysql.connection.commit()
-        cursor.close()
+        connection = mysql.connector.connect(host='localhost',database='coreai',user='root',password='')
+        cursor = connection.cursor()
+        cursor.execute(''' INSERT INTO dataset VALUES(%s,%s,%s,%s,%s,%s,%s)''',(data_id,amount,amount_one,amount_two,amount_three,amount_four,date))
+        connection.commit()
+        connection.close()
         return f"Done!!"
+    
 
 except Error as e:
     print("Error while connecting to Mysql", e)
-    
 finally:
     connection.close()
     print (" mysql closed")
