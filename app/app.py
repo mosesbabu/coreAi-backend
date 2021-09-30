12cbc22766bae77034a168d9f5cb80b634656973
moselamar <44118179+moselamar@users.noreply.github.com>
2021-09-30 22:23:33 +0300

    change amount to separate model with date

--- app/app.py
+++ app/app.py
@@ -10,9 +10,11 @@ app = Flask(__name__)
 
 
 try:
+    #establish connection to mysql server
     connection = mysql.connector.connect(host='localhost',database='coreai',user='root',password='')
     if connection.is_connected():
-        SQL = "select id,amount,amount_one,amount_two,amount_three,amount_four,date from dataset;"
+        SQL = "select id,amount,amount_one,amount_two,amount_three,amount_four from amount;"
+        
         cursor = connection.cursor()
         cursor.execute(SQL)
         #returns sql result
@@ -31,6 +33,8 @@ try:
     for i in fields_list:
         column_list.append(i[0])
     print("print final column_list",column_list)
+    
+    
 
         # prints the column list
     jsonData_list = []
@@ -41,11 +45,46 @@ try:
        
         jsonData_list.append(data_dict)
     
+    connection = mysql.connector.connect(host='localhost',database='coreai',user='root',password='')
+    if connection.is_connected():
+        SQL = "select id,date,date_one,date_two,date_three,date_four from dates;"
+        
+        cursor = connection.cursor()
+        cursor.execute(SQL)
+        #returns sql result
+        data = cursor.fetchall()      
+        print("fetch result-->",type(data))  #is s list type, need to be a dict
+
+        fields_list = cursor.description   # sql key name
+        print("fields result -->",type(fields_list))
+        cursor.close()
+        connection.close()
 
 
+
+   # main function
+    date_list = []
+    for i in fields_list:
+        date_list.append(i[0])
+    print("print final date_list",date_list)
+    
+    
+
+        # prints the column list
+    jsonDate_list = []
+    for row in data:
+        data_dict = {}
+        for i in range(len(date_list)):
+            data_dict[date_list[i]] = row[i]
+       
+        jsonDate_list.append(data_dict)
+    #api endpoint route to create retreive all dataset records from the db
     @app.route('/api/retrieve', methods=['GET'])
     def get_data():
-        return jsonify({'dataset': jsonData_list})
+        return jsonify({'dataset': jsonData_list, 'label':jsonDate_list})
+    
+    #api endpoint route to create dataset record and save to db
+    
     @app.route('/api/add', methods = ['POST'])
     def create():
         data_id=request.json['id']
@@ -60,6 +99,21 @@ try:
         cursor.execute(''' INSERT INTO dataset VALUES(%s,%s,%s,%s,%s,%s,%s)''',(data_id,amount,amount_one,amount_two,amount_three,amount_four,date))
         connection.commit()
         connection.close()
+        return f"success!!"
+    #api endpoint route to delete dataset record
+    
+    @app.route('/api/delete/<id>', methods=['DELETE'])
+    def delete_data(id):
+        connection = mysql.connector.connect(host='localhost',database='coreai',user='root',password='')
+        SQL = "DELETE FROM dataset WHERE id=id ;"
+        cursor = connection.cursor()
+        cursor.execute(SQL)
+        cursor.close()
+        connection.close()
+
+        
+        
+        
         return f"Done!!"
     
 
--- app/models.py
+++ app/models.py
@@ -8,16 +8,23 @@ Base = declarative_base()
 metadata = Base.metadata
 
 class Dataset(Base):
-    __tablename__ = 'dataset'
+    __tablename__ = 'amount'
     id = Column(INTEGER,primary_key = True)
     amount = Column(String(16))
     amount_one = Column(String(16))
     amount_two = Column(String(16))
     amount_three = Column(String(16))
     amount_four = Column(String(16))
-    date = Column(String(16))
     
-
+class Dates(Base):
+    __tablename__ = 'dates'
+    id = Column(INTEGER,primary_key = True)
+    date = Column(String(16))
+    date_one = Column(String(16))
+    date_two = Column(String(16))
+    date_three = Column(String(16))
+    date_four = Column(String(16))
+    
 
 
 
